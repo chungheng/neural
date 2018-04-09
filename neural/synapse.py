ccab206880a789398662f49d4a69dcf002d9e15e
chungheng <chungheng.yeh@gmail.com>
2018-04-09 14:44:02 -0400

    tune the parameters of the synapse models.

--- neural/synapse.py
+++ neural/synapse.py
@@ -9,55 +9,75 @@ class AMPA(Model):
     """
     AMPA Synapse
     """
-    Default_States = dict(s=0.)
-    Default_Params = dict(ar=0.5, ad=0.19, gmax=1.)
+    Default_States = dict(s=(0., 0., 1.))
+    Default_Params = dict(ar=1e-1, ad=5e0, gmax=150.)
 
-    def ode(self, spike, **kwargs):
-        self.d_s = self.ar*spike*(1.-self.s) - self.ad*self.s
+    def ode(self, stimulus, **kwargs):
+        self.d_s = -self.ad*self.s
+
+        if stimulus:
+            self.s += self.ar*(1.-self.s)
 
 class NMDA(Model):
     """
     NMDA Synapse
     """
-    Default_States = dict(x=0, s=0)
-    Default_Params = dict(a1=0.09, b1=0.0012, a2=0.18, b2=0.034, gmax=1)
+    Default_States = dict(x=(0., 0., 1.), s=(0., 0., 1.))
+    Default_Params = dict(a1=5e1, b1=4.5e0, a2=1.8e1, b2=3.4e1, gmax=300.)
 
-    def ode(self, spike, **kwargs):
-        self.d_x = self.a1*spike*(1.-self.x) - self.b1*self.x
+    def ode(self, stimulus, **kwargs):
+        self.d_x = -self.b1*self.x
         self.d_s = self.a2*self.x*(1.-self.s) - self.b2*self.s
+
+        if stimulus:
+            self.x += self.a1*(1.-self.x)
 
 class GABAB(Model):
     """
     GABA_B Synapse
     """
-    Default_States = dict(r=0, s=0)
-    Default_Params = dict(ar=0.09, br=0.012, k3=0.18, k4=0.034, kd=100, gmax=1, n=4)
+    Default_States = dict(r=(0., 0., 1.), s=(0., 0., 1.))
+    Default_Params = dict(ar=1e1, br=1e1, k3=1.8e1, k4=3.4e1, kd=.4, gmax=200., n=4)
 
-    def ode(self, spike, **kwargs):
-        self.d_r = self.ar*spike*(1.-self.r) - self.br*self.r
+    def ode(self, stimulus, **kwargs):
+        self.d_r = -self.br*self.r
         self.d_s = self.k3*self.r - self.k4*self.s
 
+        if stimulus:
+            self.r += self.ar*(1.-self.r)
+
     def get_conductance(self):
-        return self.gmax*self.s
+        p = self.s ** self.n
+        c = self.kd ** self.n
+        return self.gmax * p / (p + c)
 
 class Exponential(Model):
     """
     Exponential Synapse
     """
     Default_States = dict(s=0.)
-    Default_Params = dict(a1=1.1, b1=.19, gmax=1)
+    Default_Params = dict(a1=1.1e1, b1=9e0, gmax=1.)
 
-    def ode(self, spike, **kwargs):
-        self.d_s = self.a1*spike - self.b1*self.s
+    def ode(self, stimulus, **kwargs):
+        self.d_s = -self.b1*self.s
+
+        if stimulus:
+            self.s += self.a1
+
+    def get_conductance(self):
+        return self.s
 
 class Alpha(Model):
     """
     Alpha Synapse
     """
     Default_States = dict(s=0., u=0.)
-    Default_Params = dict(ar=12.5, ad=12.19, gmax=1)
+    Default_Params = dict(ar=1.25e1, ad=12.19, gmax=1.)
 
-    def ode(self, spike, **kwargs):
+    def ode(self, stimulus, **kwargs):
         self.d_s = self.u
         tmp = self.ar*self.ad
-        self.d_u = -(self.ar+self.ad)*self.u - tmp*self.s + tmp*spike
+        self.d_u = -(self.ar+self.ad)*self.u - tmp*self.s
+
+        if stimulus:
+            self.u += tmp
