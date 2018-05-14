83a508d95ec73c84d0afb897e4ffb86bac9c4203
chungheng <chungheng.yeh@gmail.com>
2018-05-13 23:08:54 -0400

    use gpu buffer in recorder; gain 3x speed improvement

--- neural/recorder.py
+++ neural/recorder.py
@@ -6,7 +6,6 @@ import pycuda
 import pycuda.gpuarray as garray
 import pycuda.driver as cuda
 
-
 class CUDARecorder(object):
     """
     Recorder for reading CUDA arrays of Neural models.
@@ -15,16 +14,38 @@ class CUDARecorder(object):
         self.model = model
         self.steps = steps
         self.num = model.cuda_kernel.num
-        self.dct = {key: np.zeros((self.num, self.steps)) for key in attrs}
 
-        if gou_buffer:
-            self.gpu_dct = garray.zeros((self.num, self.steps))
+        if gpu_buffer:
+            self.gpu_dct = {}
+            for key in attrs:
+                dtype = self.model.gdata[key].dtype
+                self.gpu_dct[key] = garray.zeros((self.steps, self.num), dtype)
+            self.copy_memory = self._copy_memory_dtod
+            self.dct = {key: None for key in attrs}
+        else:
+            self.dct = {key: np.zeros((self.num, self.steps)) for key in attrs}
+            self.copy_memory = self._copy_memory_dtoh
 
     def __iter__(self):
         for i in xrange(self.steps):
+            self.copy_memory(i)
+            yield i
+
+    def _copy_memory_dtod(self, index):
+        for key in self.dct.keys():
+            size = self.model.gdata[key].nbytes
+            src = self.model.gdata[key].gpudata
+            dest = int(self.gpu_dct[key].gpudata) + size*index
+            # print src, dest
+            cuda.memcpy_dtod(dest, src, size)
+        if index == self.steps:
             for key in self.dct.keys():
-                self.dct[key][:,i] = getattr(self.model, key).get()
-                yield i
+                self.dct[key] = self.gpu_dct[key].get().T
+            # self.dct[key][:,index] = getattr(self.model, key).get()
+
+    def _copy_memory_dtoh(self, index):
+        for key in self.dct.keys():
+            self.dct[key][:,index] = getattr(self.model, key).get()
 
     def __getitem__(self, key):
         return self.dct[key]
