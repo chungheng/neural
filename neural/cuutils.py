7da4ca95c2528496ad460d8d81c803d48f73423e
chungheng <chungheng.yeh@gmail.com>
2018-05-17 12:59:41 -0400

    split random seed initialization and random number generatation into two kernels

--- neural/cuutils.py
+++ neural/cuutils.py
@@ -1,22 +1,39 @@
 import numpy as np
 
+import pycuda
 import pycuda.driver as cuda
 import pycuda.gpuarray as gpuarray
 import pycuda.curandom
+
 from pycuda.compiler import SourceModule
 
-g_spk_generate_cuda = """
+src_cuda = """
 #include <cuda.h>
 #include <curand.h>
 #include <curand_kernel.h>
 
 extern "C"{
 
-__global__ void  spike_generate(
+
+__global__ void  generate_seed(
+    int num,
+    curandState *seed
+)
+{
+    int tid = threadIdx.x + blockIdx.x * blockDim.x;
+    int total_threads = gridDim.x * blockDim.x;
+
+    for (int i = tid; i < num; i += total_threads)
+        curand_init(clock64(), i, 0, &seed[i]);
+
+    return;
+}
+
+__global__ void  generate_spike(
     int num,
     double dt,
     double rate,
-    curandState_t *seed,
+    curandState *seed,
     double *scale,
     double *spike
 )
@@ -24,17 +41,11 @@ __global__ void  spike_generate(
     int tid = threadIdx.x + blockIdx.x * blockDim.x;
     int total_threads = gridDim.x * blockDim.x;
 
-    double r;
-
-    curandState_t state;
-
+    float r;
     for (int i = tid; i < num; i += total_threads) {
-        state = seed[i];
-
-        r = curand_uniform(&state);
+        r = (curand_uniform(&seed[i]));
 
         spike[i] = double(r < dt*rate*scale[i]);
-        seed[i] = state;
     }
 
     return;
@@ -42,12 +53,15 @@ __global__ void  spike_generate(
 }
 """
 
-mod = SourceModule(g_spk_generate_cuda,
+mod = SourceModule(src_cuda,
     options = ["--ptxas-options=-v"],
     no_extern_c = True
 )
-_spk_generate = mod.get_function("spike_generate")
-_spk_generate.prepare('iDDPPP')
+_generate_spike = mod.get_function("generate_spike")
+_generate_spike.prepare('iddPPP')
+
+_generate_seed = mod.get_function("generate_seed")
+_generate_seed.prepare('iP')
 
 class CUDASpikeGenerator(object):
     def __init__(self, dt, num, scale, **kwargs):
@@ -56,22 +70,23 @@ class CUDASpikeGenerator(object):
         self.dt = dt
         self.num = num
         self.scale = scale
-        self.scale.astype(self.dtype)
+        self.scale = self.scale.astype(self.dtype)
 
-        self.gpu_seed = pycuda.curandom.seed_getter_unique(self.num)
+        self.gpu_seed = pycuda.driver.mem_alloc(num * 48)
         self.gpu_scale = gpuarray.to_gpu(self.scale)
-        self.spike = gpuarray.empty_like(self.gpu_scale)
+        self.spike = gpuarray.zeros(num, dtype=self.dtype)
 
-        self.block = (256,1,1)
+        self.block = (128,1,1)
         self.grid = (min(6 * cuda.Context.get_device().MULTIPROCESSOR_COUNT,
-                    (self.num-1) / 256 + 1), 1)
-                    
-        self._spk_generate = mod.get_function("spike_generate")
+                    (self.num-1) / self.block[0] + 1), 1)
 
+        self._generate_spike = _generate_spike
+        self._generate_seed = _generate_seed
+        self._generate_seed.prepared_async_call(
+            self.grid, self.block, None, np.int32(self.num), self.gpu_seed)
 
 
     def generate(self, rate):
-        self._spk_generate.prepared_async_call(
-            self.grid, self.block, None, self.num, self.dt, self.dtype(rate),
-            self.gpu_seed.gpudata, self.gpu_scale.gpudata, self.spike.gpudata)
-        return self.spike
+        self._generate_spike.prepared_async_call(
+            self.grid, self.block, None, np.int32(self.num), self.dt, rate,
+            self.gpu_seed, self.gpu_scale.gpudata, self.spike.gpudata)
