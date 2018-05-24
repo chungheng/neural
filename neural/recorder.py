96e3a52789e974ce9f1cc0487a4fe4aa7e007795
chungheng <chungheng.yeh@gmail.com>
2018-05-23 23:48:52 -0400

    experiment around GPU to CPU memory copy

--- neural/recorder.py
+++ neural/recorder.py
@@ -1,6 +1,7 @@
 """
 Utility modules for recording data from Neural models
 """
+import time
 import numpy as np
 import pycuda
 import pycuda.gpuarray as garray
@@ -42,21 +43,32 @@ class CUDARecorder(object):
     """
     Recorder for reading CUDA arrays of Neural models.
     """
-    def __init__(self, model, attrs, steps, gpu_buffer=False):
+    def __init__(self, model, attrs, steps, **kwargs):
         self.model = model
         self.steps = steps
         self.num = model.cuda_kernel.num
+        gpu_buffer = kwargs.pop('gpu_buffer', False)
 
         if gpu_buffer:
+            self.buffer_length = self._get_buffer_length(gpu_buffer)
             self.gpu_dct = {}
+            self.dct = {}
             for key in attrs:
                 dtype = self.model.gdata[key].dtype
-                self.gpu_dct[key] = garray.zeros((self.num, self.steps), dtype)
+                # self.gpu_dct[key] = garray.zeros((self.num, self.buffer_length), dtype)
+                self.gpu_dct[key] = garray.zeros((self.buffer_length, self.num), dtype)
+                self.dct[key] = np.zeros((self.num, self.steps), order='F', dtype=dtype)
+                # self.dct[key] = []
             self.copy_memory = self._copy_memory_dtod
-            self.dct = {key: None for key in attrs}
+            # self.dct = {key: None for key in attrs}
         else:
             self.dct = {key: np.zeros((self.num, self.steps)) for key in attrs}
             self.copy_memory = self._copy_memory_dtoh
+
+        # self.start = cuda.Event()
+        # self.end = cuda.Event()
+        # self.time_dd = 0.
+        # self.time_hd = 0.
 
         self.block = (256, 1, 1)
         self.grid = (min(6 * cuda.Context.get_device().MULTIPROCESSOR_COUNT,
@@ -67,17 +79,52 @@ class CUDARecorder(object):
             self.copy_memory(i)
             yield i
 
+    def _get_buffer_length(self, gpu_buffer):
+        if gpu_buffer == 'full' or gpu_buffer == 'whole' or gpu_buffer is True:
+            return self.steps
+        else:
+            return min(gpu_buffer, self.steps)
+
     def _copy_memory_dtod(self, index):
         for key in self.dct.keys():
             dtype = dtype_to_ctype(self.model.gdata[key].dtype)
             src = self.model.gdata[key].gpudata
+            nbytes = self.model.gdata[key].nbytes
             dst = int(self.gpu_dct[key].gpudata)
-            _copy[dtype].prepared_async_call(self.grid, self.block, None,
-                self.num, index, self.steps, dst, src)
 
-        if index == self.steps-1:
+            idx = index % self.buffer_length
+            # self.start.record()
+            # _copy[dtype].prepared_call(self.grid, self.block,
+            #     self.num, idx, self.buffer_length, dst, src)
+            cuda.memcpy_dtod(dst + idx * nbytes, src, nbytes)
+            # self.end.record()
+
+            # self.end.synchronize()
+            # self.time_dd += self.start.time_till(self.end)*1e-3
+
+
+        end = index + 1
+
+        if index == self.steps-1 or (end % self.buffer_length == 0):
+
             for key in self.dct.keys():
-                self.dct[key] = self.gpu_dct[key].get()
+                beg = (index / self.buffer_length) * self.buffer_length
+                nbytes = self.gpu_dct[key].nbytes / self.buffer_length
+                offset = beg * nbytes
+                size = (end-beg) * nbytes
+                # val = self.gpu_dct[key].get()
+                # self.start.record()
+                # self.dct[key][:, beg:end] = val[:, :end-beg]
+                # self.dct[key].append( val )
+                # self.dct[key][beg:end] = val[:end-beg]
+
+                buffer = np.getbuffer(self.dct[key], offset, size)
+                cuda.memcpy_dtoh(buffer, self.gpu_dct[key].gpudata)
+                # self.end.record()
+                #
+                # self.end.synchronize()
+                # self.time_hd += self.start.time_till(self.end)*1e-3
+
 
     def _copy_memory_dtoh(self, index):
         for key in self.dct.keys():
