0eedf403ac1982f20ffd86f88bf094d401f96adb
chungheng <chungheng.yeh@gmail.com>
2019-01-16 15:25:23 -0500

    use SimpleNamespace to wrap cuda related attributes

--- neural/basemodel.py
+++ neural/basemodel.py
@@ -9,6 +9,19 @@ from six import StringIO, with_metaclass
 import numpy as np
 
 try:
+    from types import SimpleNamespace
+except:
+    class SimpleNamespace (object):
+        def __init__ (self, **kwargs):
+            self.__dict__.update(kwargs)
+        def __repr__ (self):
+            keys = sorted(self.__dict__)
+            items = ("{}={!r}".format(k, self.__dict__[k]) for k in keys)
+            return "namespace({})".format(", ".join(items))
+        def __eq__ (self, other):
+            return self.__dict__ == other.__dict__
+
+try:
     from optimizer import OdeGenerator
 except ImportError:
     OdeGenerator = None
