9cbdb054d8dcd0e8507f16628030e35653c98c6e
Tingkai Liu <tl2747@columbia.edu>
2020-12-26 21:44:27 -0500

    add more error and warning classes

--- neural/logger.py
+++ neural/logger.py
@@ -1,17 +1,55 @@
-import logging as logger
-
-logger.basicConfig(format="%(asctime)s %(message)s", level=logger.INFO)
+class NeuralWarning(Warning):
+    """Base Neural Warning Class"""
 
 
 class NeuralError(Exception):
-    pass
+    """Base Neural Exception Class"""
+
+class NeuralBackendError(NeuralError):
+    """Backend Exception"""
+
+class NeuralBackendWarning(NeuralWarning):
+    """Backend Warning"""
+
+class NeuralNetworkError(NeuralError):
+    """Neural Error for Network Module"""
+
+
+class NeuralNetworkCompileError(NeuralNetworkError):
+    """Neural Network Compilation Error"""
+
+
+class NeuralNetworkInputError(NeuralNetworkError):
+    """Neural Network Input Object Error"""
+
+
+class NeuralNetworkUpdateError(NeuralNetworkError):
+    """Container's Update Faield in Neural Network"""
 
 
 class NeuralUtilityError(NeuralError):
-    pass
+    """Neural Error for Utility Functions
+
+    Includes:
+
+        1. `utils.py`
+        2. `plot.py`
+    """
 
 
 class SignalTypeNotFoundError(NeuralUtilityError):
     """Raised for when generating an unknown stimulus"""
 
-    pass
+
+class NeuralUtilityWarning(NeuralWarning):
+    """Neural Error for Utility Functions
+
+    Includes:
+
+        1. `utils.py`
+        2. `plot.py`
+    """
+
+
+class NeuralNetworkWarning(NeuralWarning):
+    """Neural Error for Network Module unctions"""
