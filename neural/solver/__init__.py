dec07fa426775b62c1a78c69a423e798afc345ed
Tingkai Liu <tl2747@columbia.edu>
2022-03-27 21:47:04 -0400

    cleanup solver init

--- neural/solver/__init__.py
+++ neural/solver/__init__.py
@@ -2,19 +2,9 @@
 """
 import collections
 from .basesolver import BaseSolver, Euler
-from ._scipy import (
-    RK45Solver,
-    RK23Solver,
-    DOP853Solver,
-    RadauSolver,
-    LSODASolver,
-)
 
-SOLVERS = collections.namedtuple("Solvers", "euler RK45 RK23 DOP853 Radau LSODA")(
+# from ._numba import NumbaEulerSolver, NumbaMidpointSolver, NumbaSolver
+
+SOLVERS = collections.namedtuple("Solvers", "euler")(
     Euler,
-    RK45Solver,
-    RK23Solver,
-    DOP853Solver,
-    RadauSolver,
-    LSODASolver,
 )
